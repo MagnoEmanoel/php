99490db9eb037a45697bb871ae5aa5bdc9a4d862
Magno Emanoel <magno.emanoel@unimedmaranhaodosul.coop.br>
2024-06-17 09:52:40 -0300

    Add strings.php to version control

---
+++ strings.php
@@ -0,0 +1,13 @@
+<?php
+
+$idade = 25; 
+//echo 'ola mundo! minha idade é ' . $idade . ' anos'; // quando vc usa aspas simples
+                                                      // voce tem que concatenar EX: 'ola mundo! minha idade é ' . $idade . ' anos'
+                                                     
+echo "ola mundo! minha idade é $idade anos\n"; // Quando se usa aspas duplas vc poe inserir variaveis sem concatenar
+
+//echo "ola mundo! minha idade é $idade anos\n" --- a linha é quebranda insrindo a varivael \n no da
+// string ou a baixo ex: echo "\n" já um tabe usa "\t"
+
+echo "quebra de linha"; 
+                                             
